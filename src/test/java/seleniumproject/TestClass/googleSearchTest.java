5f1387412b6fda3faf1a57bb5690b1592eee987e
Vandanatiwari290 <vandanatiwari290@gmail.com>
2021-01-02 21:07:12 +0530

    small changes

--- src/test/java/seleniumproject/TestClass/googleSearchTest.java
+++ src/test/java/seleniumproject/TestClass/googleSearchTest.java
@@ -7,13 +7,15 @@ import java.util.List;
 public class googleSearchTest extends BaseTest{
     @Test
     public void testcase1(){
-     driver.findElement(By.name("q")).sendKeys("testing");
+
+        System.out.println(driver.getTitle());
+     /*driver.findElement(By.name("q")).sendKeys("testing");
       List<WebElement>list=  driver.findElements(By.xpath("//ul[@role='listbox']//li/descendant::div[@class='sbtc']"));
         System.out.println("Total number of suggestion is:"+list.size());
         for (int i=0;i<list.size();i++){
             String text=list.get(i).getText();
-            System.out.println(text);
+            System.out.println(text);*/
         }
     }
 
-}
+
